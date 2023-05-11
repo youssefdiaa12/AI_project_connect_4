686dab527e2d6cfadff63e874169285753f4afef
Abdallah Shouker <81974700+AbdallahShouker@users.noreply.github.com>
2023-05-11 06:09:09 +0300

    Update main.py
    - improve AI actions in game to block player winning ways if winning position vertical or diagonal  
    - added score system to AI for evaluating best move

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ PLAYER_PIECE = 1
 AI_PIECE = 2
 
 EMPTY = 0
+WINDOW_LENGTH = 4
 
 
 def create_board():
@@ -71,18 +72,58 @@ def wining_move(board, piece):
                 return True
 
 
+def evaluate_window(window, piece):
+	score = 0
+	opp_piece = PLAYER_PIECE
+	if piece == PLAYER_PIECE:
+		opp_piece = AI_PIECE
+
+	if window.count(piece) == 4:
+		score += 100
+	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+		score += 10
+	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+		score += 5
+
+	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+		score -= 80
+
+	return score
+
 def score_position(board, piece):
-    # score horizontal
-    score = 0
-    for r in range(ROW_COUNT):
-        row_array = [int(x) for x in (board[r, :])]
-        for i in range(COLUMN_COUNT-3):
-            window = row_array[i:i+4]
-            if window.count(piece) == 4:
-                score += 100
-            elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-                score += 10
-    return score
+	score = 0
+
+	## Score center column
+	center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+	center_count = center_array.count(piece)
+	score += center_count * 6
+
+	## Score Horizontal
+	for r in range(ROW_COUNT):
+		row_array = [int(i) for i in list(board[r,:])]
+		for c in range(COLUMN_COUNT-3):
+			window = row_array[c:c+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score Vertical
+	for c in range(COLUMN_COUNT):
+		col_array = [int(i) for i in list(board[:,c])]
+		for r in range(ROW_COUNT-3):
+			window = col_array[r:r+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score posiive sloped diagonal
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	return score
 
 
 def get_valid_locations(board):
@@ -95,7 +136,7 @@ def get_valid_locations(board):
 
 def pick_best_move(board, piece):
     valid_locations = get_valid_locations(board)
-    best_score = 0
+    best_score = -10000
     best_col = random.choice(valid_locations)
     for col in valid_locations:
         row = get_next_open_row(board, col)
